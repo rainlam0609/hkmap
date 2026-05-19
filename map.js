98e1c5c10bef217907d88a2b132c90e638905367
rain <rainlam0609@gmail.com>
2026-05-19 20:11:07 +0800

    Update map.js

--- map.js
+++ map.js
@@ -190,7 +190,7 @@ const restaurants = [
         location: '屯門市廣場1期',
         tier: 'S Tier',
         sizeRating: 1,
-        coords: [22.33118675830544, 114.15993402879299],
+        coords: [22.392751476166243, 113.97794172431637],
         url: 'https://www.openrice.com/zh/hongkong/r-%E5%A4%A7%E9%96%80%E6%8B%89%E9%BA%B5-%E5%B1%AF%E9%96%80-%E6%97%A5%E6%9C%AC%E8%8F%9C-%E6%8B%89%E9%BA%B5-r474534'
     },
     {
